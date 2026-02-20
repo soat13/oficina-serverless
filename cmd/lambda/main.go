34d1c3a2a7faf3ef42ee1311cd06d8088600e34c
ssgoncalves <samuel_gs@live.com>
2026-02-19 22:38:58 -0300

    tests

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
-
 	identitybootstrap "github.com/soat13/oficina-serverless/internal/bootstrap/identity"
 	"github.com/soat13/oficina-serverless/internal/container"
 	"github.com/soat13/oficina-serverless/internal/identity/infra/config"
@@ -15,13 +14,15 @@ func main() {
 
 	c, err := container.New(cfg)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("container init error: %v", err)
+		panic(err)
 	}
 	defer func() { _ = c.Close() }()
 
 	mod, err := identitybootstrap.Setup(c)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("bootstrap setup error: %v", err)
+		panic(err)
 	}
 
 	lambda.Start(mod.ApiGwHandler.Handle)
